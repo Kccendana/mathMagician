d6420f96acb3d0f8dc6c5fae953be17f87aaa786
kcccendana <kc_telyn@yahoo.com>
2023-08-10 00:48:40 +0900

    add onclick event to the button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import PropTypes from 'prop-types';
 
 function Button(props) {
-  const { label, className } = props;
+  const { label, className, handleButtonClick } = props;
   return (
-    <button type="button" className={className}>
+    <button type="button" className={className} onClick={handleButtonClick}>
       {label}
     </button>
   );
@@ -12,6 +12,7 @@ function Button(props) {
 Button.propTypes = {
   label: PropTypes.string.isRequired,
   className: PropTypes.string,
+  handleButtonClick: PropTypes.func.isRequired,
 };
 
 Button.defaultProps = {
