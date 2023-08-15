d6503fc4a58da10dbcf2a1be23f4d53b293be32e
kcccendana <kc_telyn@yahoo.com>
2023-08-15 12:08:01 +0900

    add home component

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home-wrapper">
+      <h1>Welcome to MathMagician</h1>
+      <p className="home-content">Explore the realm of possibilities by engaging in calculations and drawing inspiration from our uniquely generated quotes, tailored exclusively for you.</p>
+    </div>
+  );
+}
+
+export default Home;
