dbc541f3813d4e6746a67be605abf56a2f77c051
kcccendana <kc_telyn@yahoo.com>
2023-08-15 12:11:02 +0900

    modify and fix error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Nav from './components/Nav';
 import Calculator from './components/Calculator';
 import Quote from './components/Quote';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quote />
-    </div>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="calculator" element={<Calculator />} />
+        <Route path="quote" element={<Quote />} />
+      </Routes>
+    </>
   );
 }
 
